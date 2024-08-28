de9a1771b2a8ddfb678d0e6855aa5a86ff5e1103
Luis Perez <lperezdev843@gmail.com>
2024-08-28 18:24:26 -0400

    finsihed register users route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,25 +1,25 @@
+const bcrypt = require('bcrypt')
+const _ = require("lodash");
 const { User, validate } = require("../model/user");
 const express = require("express");
 const router = express.Router();
 
 // register user
 router.post("/", async (req, res) => {
-//   const { error } = validate(req.body);
-//   if (error) return res.status(400).send(error.details[0].message);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
   if (user) return res.status(400).send("User already registered");
 
-  user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  user = new User(
+    _.pick(req.body, ["firstName", "lastName", "username", "email", "password"])
+  );
+  const salt = await bcrypt.genSalt(12);
+  user.password = await bcrypt.hash(user.password, salt);
 
   await user.save();
-  res.send(user);
+  res.send(_.pick(user, ["_id", "firstName", "email"]));
 });
 
 module.exports = router;
