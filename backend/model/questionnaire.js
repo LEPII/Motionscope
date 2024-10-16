de734deb4007f87040ea309a678923d1cd2b5025
Luis Perez <lperezdev843@gmail.com>
2024-10-16 14:37:08 -0400

    finished schema for questionnaire, just needs testing

--- backend/model/questionnaire.js
+++ backend/model/questionnaire.js
@@ -175,11 +175,84 @@ const Questionnaire = mongoose.model("Questionnaire", questionnaireSchema);
 
 function validateQuestions(questions) {
   const schema = {
-    // firstName: Joi.string()
-    birthday: Joi.date(),
-    gender: Joi.string(),
-    
+    birthday: Joi.date()
+      .required()
+      .custom((value, helper) => {
+        if (value > Date.now()) {
+          throw new Joi.ValidationError("birthday", "must be in the past");
+        }
+        return value;
+      }),
+    gender: Joi.string().valid("Male", "Female").required(),
+    preferredMetric: Joi.string().valid("kilograms", "pounds").required(),
+    height: Joi.number().required(),
+    currentBodyWeight: Joi.number().min(10).max(500).required(),
+    desiredWeightClass: Joi.string()
+      .valid("59kg", "66kg", "74kg", "83kg", "93kg", "105kg", "120kg", "120kg+")
+      .required(),
+    gymExperienceYears: Joi.number().min(0).max(100).required(),
+    powerliftingExperienceYears: Joi.number().min(0).max(100).required(),
+    competedInMeet: Joi.boolean().required(),
+    bestSquatTraining: Joi.number().default(null).min(1).max(2000).required(),
+    bestBenchTraining: Joi.number().default(null).min(1).max(2000).required(),
+    bestDeadliftTraining: Joi.number()
+      .default(null)
+      .min(1)
+      .max(2000)
+      .required(),
+    bestSquatCompetition: Joi.number().default(null).min(1).max(2000),
+    bestBenchCompetition: Joi.number().default(null).min(1).max(2000),
+    bestDeadliftCompetition: Joi.number().default(null).min(1).max(2000),
+    nextPlannedCompetitionDate: Joi.date()
+      .default(null)
+      .custom((value) => {
+        if (value && value < Date.now()) {
+          throw new Joi.ValidationError(
+            "nextPlannedCompetitionDate",
+            "must be today or in the future"
+          );
+        }
+        return value;
+      }),
+    trainingHistory: Joi.string().max(2000).required(),
+    currentTrainingProgram: Joi.string().max(2000).required(),
+    rpeTrainingFamiliarity: Joi.string()
+      .valid("Not Familiar", "Somewhat Familiar", "Proficient")
+      .required(),
+    deadliftStance: Joi.string()
+      .valid("Conventional", "Sumo", "No Preference")
+      .required(),
+    liftingGoals: Joi.string().max(2000).required(),
+    weeklyTrainingDays: Joi.number().required(),
+    availableTrainingDays: Joi.array()
+      .items(
+        Joi.string().valid(
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday"
+        )
+      )
+      .required(),
+    availableEquipment: Joi.string().max(2000).required(),
+    injuries: Joi.string().max(2000).optional(),
+    startDate: Joi.date()
+      .required()
+      .custom((value) => {
+        if (value < Date.now()) {
+          throw new Joi.ValidationError(
+            "startDate",
+            "must be today or in the future"
+          );
+        }
+        return value;
+      }),
+    questionsOrConcerns: Joi.string().max(2000).optional(),
   };
+
   return Joi.validate(questions, schema);
 }
 
