a6e7efb1b43010c1e48afc64c6f3308178b0b465
Luis Perez <lperezdev843@gmail.com>
2025-01-06 22:15:38 -0500

    even more routes and reafactoring

--- backend/routes/programs.js
+++ backend/routes/programs.js
@@ -5,21 +5,18 @@ const router = express.Router();
 
 // -- COACH'S ENDPOINTS -- 
 
-// Get all Programs 
+// Get All Athletes/Programs  
 
-// Get a Single Program 
+// Get a Single Program from a specific Athlete - (Which will then show a preview of all blocks)
 
-// Post a Program 
-
-// Post Athlete to Program
+// Post a Program - Must have no more / no less than 1 athlete associated 
 
 // Update a Program
 
-// Delete a Program
+// Delete a Program / Athlete - Archive Option?  
 
 // -- ATHLETE'S ENDPOINTS --
 
 // Get Current Program
-
 
 module.exports = router;
